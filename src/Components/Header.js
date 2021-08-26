cc53b1a4855d935b791fdfafc9c10533c2d8a51a
Marcos Trejo <marcostrejo23@gmail.com>
2021-08-25 18:47:44 -0700

    navbar changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import Typed from "react-typed";
 const Header = () => {
     return (
      <div className="header-wrapper">
      <div className="main-info">
-     <h1>Test</h1>
+     <h1> I decided to pursue web development after years of being an entrepreneur. Web development being the future, 
+     I wanted to have the know-how to compete in the market. When I'm not pursuing the knowledge of web development, 
+     I'm going crazy for Seattle sports, out in the woods looking for the next best photography spot, or at the cinemas keeping up with the latest films.</h1>
+     <Typed 
+        className="typed-text"
+        strings={["Web Development", "Software Development", "Interpersonal Communication"]}
+        typeSpeed={40}
+        backSpeed={60}
+        loop
+     />
      </div>
      </div>
     )
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+
  
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-#7efff5 bg-light">
         <div className='container'>
         <a className="navbar-brand" href="#">Marcos Trejo Portfolio</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
