ad7e49723d518589477ab91660f3a70265bbe0b4
Marcos Trejo <marcostrejo23@gmail.com>
2021-08-25 20:17:01 -0700

    testing routes'

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Components/Navbar";
 import Header from "./Components/Header";
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <>
     <Navbar />
     <Header />
+    <Footer />
     </>
 
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-
+import About from "./About";
  
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-#7efff5 bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className='container'>
         <a className="navbar-brand" href="#">Marcos Trejo Portfolio</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,10 +15,10 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
         <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        <a className="nav-link">Home <span className="sr-only">(current)</span></a>
         </li>
         <li className="nav-item">
-        <a className="nav-link" href="#">About Me</a>
+        <a className="nav-link" <Route path="/" Component={About}/>About Me</a>
         </li>
         <li className="nav-item">
         <a className="nav-link" href="#">Portfolio</a>
