53a367eba9e980ea80213e44fc72618448e630d4
Marcos Trejo <marcostrejo23@gmail.com>
2021-09-04 21:05:57 -0700

    fixing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from "react";
-import PortfolioC from "./components/PortfolioC";
+import PortfolioC from "./Components/PortfolioC";
 import "bootstrap/dist/css/bootstrap.min.css"
 const App = () => <PortfolioC />;
 
