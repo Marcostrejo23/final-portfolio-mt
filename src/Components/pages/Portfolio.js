0d9a25f75804e78a4d80e315d2c81d5e981cc343
Marcos Trejo <marcostrejo23@gmail.com>
2022-02-04 23:42:21 -0800

    cleaning up the code

--- src/Components/pages/Portfolio.js
+++ src/Components/pages/Portfolio.js
@@ -62,32 +62,3 @@ export default function Portfolio(){
 
     );
 }
-
-
-{/* import React from 'react';
-import password from '../images/PasswordGenerator.png';
-import weather from '../images/WeatherDashboard.png';
-import WorkDay from '../images/WorkDayScheduler.png';
-
-export default function  Portfolio() {
-    return(
-        <div className="container">
-            <div className="text-center">
-            <h2>My work so far</h2>
-            <img className="img-fluid port=image"src={password} class= "img-thumbnail" alt="Password Generator"></img>
-            <a href="https://github.com/Marcostrejo23/password-generator-mt">Password Generator</a>
-            <p>This is a password generator I created for my Boot camp.</p>
-            <img className="img-fluid port=image "src={weather} class= "img-thumbnail" alt="Weather Dashboard "></img>
-            <a href="https://github.com/Marcostrejo23/weather-api-mt">Weather Dashboard</a>
-            <p>I created this weather dashboard where when you select a city it will give you a five day forecast.</p>
-            </div>
-            <img className="img-fluid port=image"src={WorkDay} class= "img-thumbnail" alt="WorkDay scheduler"></img>
-            <a href="https://github.com/Marcostrejo23/workday-calendar-mt">WorkDay Scheduler</a>
-            <br />
-            <p>This is a workday scheduler where a user can input tasks that will save to local storage.</p>
-        </div>
-    );
-}
-*/}
-
-
