bd6aa989d2b6acaff4381c63dbbec9ca34501db3
Marcos Trejo <marcostrejo23@gmail.com>
2022-02-09 13:24:56 -0800

    cleaner code

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,17 +1,47 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-        <footer className="footer bg-dark fixed-bottom">
-        <div className='icon-container'>
-        <a href="https://github.com/marcostrejo23" target="_blank" rel="noreferrer" ><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
-        <a href="https://www.linkedin.com/in/marcos-trejo-915923185/" target="_blank" rel="noreferrer" ><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
-        <a href="mailto: Marcostrejo23@gmail.com" target="_blank" rel="noreferrer" ><img src="https://img.icons8.com/color/48/000000/email.png" alt="Email" className="icon"/></a>
+  return (
+    <div>
+      <footer className="footer bg-dark fixed-bottom">
+        <div className="icon-container">
+          <a
+            href="https://github.com/marcostrejo23"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://img.icons8.com/color/48/000000/github--v1.png"
+              alt="Github"
+              className="icon"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/marcos-trejo-915923185/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://img.icons8.com/fluent/48/000000/linkedin.png"
+              alt="LinkedIn"
+              className="icon"
+            />
+          </a>
+          <a
+            href="mailto: Marcostrejo23@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://img.icons8.com/color/48/000000/email.png"
+              alt="Email"
+              className="icon"
+            />
+          </a>
         </div>
-        </footer>
-        </div>
-    )
-}
+      </footer>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
