2905eaf81abf50b05ce635b6b07cace9526ff6cd
Marcos Trejo <marcostrejo23@gmail.com>
2022-02-10 19:23:18 -0800

    saving edits to navbar no longer going to linkedin and about me margins

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,14 +7,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
-        <Link
-          className="navbar-brand"
-          to="https://www.linkedin.com/in/marcos-trejo-915923185/"
-          rel='noopener noreferrer'
-          target="_blank"
-        >
-          Marcos Trejo Portfolio
-        </Link>
+        <Link className="navbar-brand">Marcos Trejo Portfolio</Link>
         <button
           className="navbar-toggler"
           type="button"
